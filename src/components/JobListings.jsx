3fb5a49955ccd9d8d74ac0aa7a2b46fc68898651
GUchechi <godswilluchendu7@gmail.com>
2024-06-17 17:24:12 +0100

    added joblisting

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,17 +1,19 @@
-import jobs from "../jobs.json";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import JobListing from "./JobListing";
 
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3);
+const JobListings = ({ isHome = false }) => {
+  const JobListings = isHome ? jobs.slice(0, 3) : jobs;
+
   return (
     <div>
       <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
           <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            Browse Jobs
+            {isHome ? "Recent Jobs" : " Browse Jobs"}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentJobs.map((job) => (
+            {JobListings.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
           </div>
@@ -19,6 +21,10 @@ const JobListings = () => {
       </section>
     </div>
   );
+};
+
+JobListings.propTypes = {
+  isHome: PropTypes.boolean,
 };
 
 export default JobListings;
